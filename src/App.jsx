3980e0f3b04ae0ce01ff06ba2c3bbd74204b3ceb
jordan <jordan@gmail.com>
2025-06-18 00:02:20 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import Saludo from './Saludo'
+
+function Saludo(){
+ return <h2>Hola,Bienvenido a mi aplicación react!</h2>
+}
+
+function App() {
+ 
+  return (
+    <>
+     <img src="./src/assets/gojologo.png.jfif" className='logo' alt="logo" />
+      <h1>Welcome to my react App</h1>
+      <Saludo/>
+      <p>esta es una aplicación</p>
+    </>
+  )
+}
+
+export default App
